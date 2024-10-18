942da182672a23852e0489c0cee392c5fbdfb2b8
DevPythonOdoo <xaviergohibi@gmail.com>
2024-10-18 15:49:54 +0000

    Ajout de la fonctionnalité de  conversion et correction des rapports

--- edilac/models/models.py
+++ edilac/models/models.py
@@ -11,9 +11,6 @@ class H_Purchase(models.Model):
     _inherit = 'purchase.order'
     _description = 'Description'
 
-
-
-
     state = fields.Selection([
         ('draft', 'Demande de prix'),
         ('sent', 'Envoyé'),
@@ -29,6 +26,44 @@ class H_Purchase(models.Model):
     number_palet = fields.Integer(string='Nombre Totale de Palet',compute='_compute_total_palet',required=False, store= True)
     qte_palet = fields.Integer(string='Total Palet',required=False, store= True,related='order_line.qte_palet')
     product_qty = fields.Float(string='Total Carton',required=False, store= True, related='order_line.product_qty')
+    total_amount_devise = fields.Float(string='Total Devise(FCFA)', required=False, compute='_compute_total_amount_devise', store=True)
+
+    @api.depends('partner_id', 'tax_totals', 'currency_id')
+    def _compute_total_amount_devise(self):
+        for order in self:
+            # Initialisation du montant à zéro
+            order.total_amount_devise = 0.0
+
+            # Vérification que le partenaire a une liste de prix
+            if order.partner_id and order.partner_id.property_product_pricelist:
+                pricelist = order.partner_id.property_product_pricelist
+                pricelist_currency = pricelist.currency_id
+
+                # Calculer le montant total des taxes dans la devise du fournisseur
+                tax_total_in_pricelist_currency = sum(
+                    float(value) for value in order.tax_totals.values()
+                    if isinstance(value, (int, float)) or (
+                            isinstance(value, str) and value.replace('.', '', 1).isdigit())
+                )
+
+                # Si la devise de la liste de prix n'est pas en FCFA, convertir en FCFA
+                if pricelist_currency.name != 'FCFA':
+                    # Récupération du dernier taux d'échange basé sur l'inverse_company_rate
+                    currency_rate = self.env['res.currency.rate'].search([
+                        ('currency_id', '=', pricelist_currency.id),
+                        ('name', '<=', fields.Date.today())  # Prendre le dernier taux jusqu'à aujourd'hui
+                    ], order='name desc', limit=1)
+
+                    if currency_rate:
+                        # Récupérer l'inverse du taux d'échange pour la conversion en FCFA
+                        inverse_rate = currency_rate.inverse_company_rate
+                        # Convertir le montant total des taxes en FCFA
+                        order.total_amount_devise = tax_total_in_pricelist_currency * inverse_rate
+                else:
+                    # Si la devise est déjà en FCFA, affecter directement le montant total des taxes
+                    order.total_amount_devise = tax_total_in_pricelist_currency
+
+
 
     type = fields.Selection(
         string=_('type'),
@@ -58,8 +93,6 @@ class H_Purchase(models.Model):
             # Vérifiez si la commande a des lignes
             if 'order_line' not in vals or not vals['order_line']:
                 raise UserError("Vous ne pouvez pas créer une commande d'achat sans lignes de commande.")
-
-
 
             company_id = vals.get('company_id', self.default_get(['company_id'])['company_id'])
             # Assurez-vous que le type de prélèvement et la devise par défaut sont pris dans la bonne entreprise.
@@ -100,11 +133,10 @@ class H_Purchase(models.Model):
                 order.message_subscribe([order.partner_id.id])
         return True
 
+
     def button_action_approuve_daf(self):
         check_config = self.env['res.config.settings'].search([], limit=1)
         print(check_config)
-
-
         for rec in self:
             # Vérifiez le type de l'enregistrement
             if rec.type == 'local':
@@ -114,8 +146,6 @@ class H_Purchase(models.Model):
                 # Si le type est 'import', passez à 'approved' et confirmez
                 rec.write({'state': 'approved'})
                 rec.button_confirm()
-
-
 
 
 
@@ -178,29 +208,31 @@ class H_Purchase(models.Model):
                     partner_vals)  # Les utilisateurs d'achats n'ont pas de droits d'écriture sur `res.partner`
 
         return orders
+
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     qte_palet = fields.Integer(string='Palet/Qte', required=False, readonly=False, store=True)
     product_id = fields.Many2one(comodel_name='product.product', string='Product', required=False)
+    total_amount_devise = fields.Float(string='Total Devise(FCFA)', required=False)
 
-    @api.onchange('product_id', 'product_qty')
-    def _onchange_product_qty(self):
-        for line in self:
-            product = line.product_id.product_tmpl_id
-            if line.product_qty:  # Vérifier si la quantité est renseignée
-                if product.uom_palet_id:
-                    # Utiliser uom_palet_id pour le calcul si disponible
-                    line.qte_palet = line.product_qty / product.uom_palet_id
-                elif product.uom_conteneur_id:
-                    # Sinon, utiliser uom_conteneur_id s'il est renseigné
-                    line.qte_palet = line.product_qty / product.uom_conteneur_id
-                else:
-                    # Aucun des deux n'est renseigné, mettre qte_palet à 0
-                    line.qte_palet = 0
-            else:
-                # Si la quantité n'est pas renseignée, mettre qte_palet à 0
-                line.qte_palet = 0
+    # @api.onchange('product_id', 'product_qty')
+    # def _onchange_product_qty(self):
+    #     for line in self:
+    #         product = line.product_id.product_tmpl_id
+    #         if line.product_qty:  # Vérifier si la quantité est renseignée
+    #             if product.uom_palet_id:
+    #                 # Utiliser uom_palet_id pour le calcul si disponible
+    #                 line.qte_palet = line.product_qty / product.uom_palet_id
+    #             elif product.uom_conteneur_id:
+    #                 # Sinon, utiliser uom_conteneur_id s'il est renseigné
+    #                 line.qte_palet = line.product_qty / product.uom_conteneur_id
+    #             else:
+    #                 # Aucun des deux n'est renseigné, mettre qte_palet à 0
+    #                 line.qte_palet = 0
+    #         else:
+    #             # Si la quantité n'est pas renseignée, mettre qte_palet à 0
+    #             line.qte_palet = 0
 
 
 class ProductTemplate(models.Model):
